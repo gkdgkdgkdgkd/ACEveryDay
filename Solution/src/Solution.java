6215707e567b2d4c902b318a93180818e7805c4d
2293736867 <2293736867@qq.com>
2021-04-05 16:43:52 +0800

    Old branch

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -2,31 +2,5 @@ import java.util.Scanner;
 
 class Solution {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        while (true)
-        {
-//            System.out.println(new Solution().strStr(scanner.nextLine(),scanner.nextLine()));
-            System.out.println(scanner.nextLine().indexOf(scanner.nextLine()));
-        }
-    }
-
-    public int strStr(String haystack, String needle) {
-        if(needle == null || needle.isEmpty() || haystack == null || haystack.isEmpty())
-            return 0;
-        int sLen = haystack.length();
-        int tLen = needle.length();
-        for(int p=0;p<sLen;++p)
-        {
-            int ps = p;
-            int pt = 0;
-            while(ps<sLen && pt<tLen && needle.charAt(pt) == haystack.charAt(ps))
-            {
-                ++ps;
-                ++pt;
-            }
-            if(pt == tLen)
-                return p;
-        }
-        return -1;
     }
 }
