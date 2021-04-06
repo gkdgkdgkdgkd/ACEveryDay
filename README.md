3b8173a66f2d3929f9783c00f2145097325164c9
2293736867 <2293736867@qq.com>
2021-04-06 10:43:18 +0800

    LeetCode 80

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 # 题目列表
 
 - [88](https://github.com/2293736867/ACEveryDay/tree/main/list/88)
+- [80](https://github.com/2293736867/ACEveryDay/tree/main/list/80)
--- Solution/src/Main.java
+++ Solution/src/Main.java
@@ -2,9 +2,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int [] a = {1,2,3,0,0,0};
-        int [] b = {2,5,6};
-        new Solution().merge(a,3,b,3);
-        System.out.println(Arrays.toString(a));
+        int [] nums = {0,0,1,1,1,1,2,3,3};
+        int l = new Solution().removeDuplicates(nums);
+        for (int i = 0; i < l; i++) {
+            System.out.print(nums[i]+" ");
+        }
     }
 }
