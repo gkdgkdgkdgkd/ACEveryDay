21ab5823ad3b5d0ba596278e43bc65cb9d4c9855
2293736867 <2293736867@qq.com>
2021-04-10 10:23:05 +0800

    LeetCode 263

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 - [81](https://github.com/2293736867/ACEveryDay/tree/main/list/81)
 - [153](https://github.com/2293736867/ACEveryDay/tree/main/list/153)
 - [154](https://github.com/2293736867/ACEveryDay/tree/main/list/154)
+- [263](https://github.com/2293736867/ACEveryDay/tree/main/list/263)
--- Solution/src/Main.java
+++ Solution/src/Main.java
@@ -1,11 +1,10 @@
 import javax.swing.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println(new Solution().findMin(new int[]{1,3,5,0,0,0,0,1}));
+    public static void main(String[] args) throws Exception {
+        System.out.println(new Solution().isUgly(9));
     }
 }
