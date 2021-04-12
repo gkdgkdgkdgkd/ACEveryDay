c12962a14c2c8bd89ce0478c3c62c99a1b779b8b
2293736867 <2293736867@qq.com>
2021-04-12 14:59:52 +0800

    LeetCode 179

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 - [154](https://github.com/2293736867/ACEveryDay/tree/main/list/154)
 - [263](https://github.com/2293736867/ACEveryDay/tree/main/list/263)
 - [264](https://github.com/2293736867/ACEveryDay/tree/main/list/264)
+- [179](https://github.com/2293736867/ACEveryDay/tree/main/list/179)
--- Solution/src/Main.java
+++ Solution/src/Main.java
@@ -1,7 +1,10 @@
-import java.util.LinkedList;
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.println(new Solution().nthUglyNumber(1000));
+    public static void main(String[] args) {
+        System.out.println(new Solution().largestNumber(new int[]{999999,0,999,88,9999999,2}));
     }
 }
