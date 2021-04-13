918cfa492976a28061358266471c192b045059c9
2293736867 <2293736867@qq.com>
2021-04-13 09:13:23 +0800

    LeetCode 783

--- Solution/src/Main.java
+++ Solution/src/Main.java
@@ -5,6 +5,6 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Solution().largestNumber(new int[]{999999,0,999,88,9999999,2}));
+        System.out.println(new Solution().lengthOfLongestSubstring("dvdf"));
     }
 }
