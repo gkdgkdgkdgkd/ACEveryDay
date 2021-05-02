85750808f0f08d70c5bdc8d3d3b2120619823076
2293736867 <2293736867@qq.com>
2021-05-02 15:34:07 +0800

    LeetCode 690

--- README.md
+++ README.md
@@ -26,3 +26,4 @@
 - [633](https://github.com/2293736867/ACEveryDay/tree/main/list/633)
 - [403](https://github.com/2293736867/ACEveryDay/tree/main/list/403)
 - [137](https://github.com/2293736867/ACEveryDay/tree/main/list/137)
+- [690](https://github.com/2293736867/ACEveryDay/tree/main/list/690)
--- Solution/src/Main.java
+++ Solution/src/Main.java
@@ -2,6 +2,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println(new Solution().singleNumber(new int[]{43,16,45,89,45,-2147483648,45,2147483646,-2147483647,-2147483648,43,2147483647,-2147483646,-2147483648,89,-2147483646,89,-2147483646,-2147483647,2147483646,-2147483647,16,16,2147483646,43}));
+        System.out.println();
     }
 }
