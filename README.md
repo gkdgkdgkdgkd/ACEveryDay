d30d3ca22f31165676ae563f8aa48d4549c992eb
2293736867 <2293736867@qq.com>
2021-08-31 07:18:48 +0800

    LeetCode 1109

--- README.md
+++ README.md
@@ -145,3 +145,4 @@
 - [1480](https://github.com/2293736867/ACEveryDay/tree/main/list/1480)
 - [1588](https://github.com/2293736867/ACEveryDay/tree/main/list/1588)
 - [528](https://github.com/2293736867/ACEveryDay/tree/main/list/528)
+- [1109](https://github.com/2293736867/ACEveryDay/tree/main/list/1109)
--- Solution/src/Main.java
+++ Solution/src/Main.java
@@ -16,5 +16,15 @@ public class Main {
     private final static Solution SOLUTION = new Solution();
 
     public static void main(String[] args) throws Exception {
+        int len = (100_00 << 1);
+        int[][] bookings = new int[len][];
+        for (int i = 0; i < len; i++) {
+            bookings[i] = new int[3];
+            bookings[i][0] = Utils.randomInt(1, 100_00);
+            bookings[i][1] = Utils.randomInt(bookings[i][0] + 1, 100_00 << 1);
+            bookings[i][2] = Utils.randomInt(1,100_00);
+        }
+        Utils.print(bookings);
+//        Utils.print(SOLUTION.corpFlightBookings(bookings,len));
     }
 }
