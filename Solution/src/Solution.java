ac2a2baf910c821534afa7bcb07f04b907a3e7ae
2293736867 <2293736867@qq.com>
2021-09-03 18:35:52 +0800

    LeetCode 面试题17.14

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,23 +1,42 @@
-import node.ListNode;
-
 import java.util.*;
 
 public class Solution {
-    public ListNode sortList(ListNode head) {
-        List<Integer> list = new ArrayList<>();
-        ListNode temp = head;
-        while (temp != null){
-            list.add(temp.val);
-            temp = temp.next;
-        }
+    private static final Random random = new Random();
 
-        list.sort(Comparator.naturalOrder());
-        temp = head;
-        int index = 0;
-        while (temp != null){
-            temp.val = list.get(index++);
-            temp = temp.next;
+    public int[] smallestK(int[] arr, int k) {
+        quickSort(arr, 0, arr.length - 1, k);
+        return Arrays.copyOfRange(arr, 0, k);
+    }
+
+    private void quickSort(int[] arr, int l, int r, int k) {
+        if (l >= r) {
+            return;
         }
-        return head;
+        int i = l;
+        int j = r;
+        swap(arr, random.nextInt(r - l + 1) + l, l);
+        int x = arr[l];
+        while (i < j) {
+            while (i < j && arr[j] >= x) {
+                --j;
+            }
+            while (i < j && arr[i] <= x) {
+                ++i;
+            }
+            swap(arr, i, j);
+        }
+        swap(arr, i, l);
+        if (i > k) {
+            quickSort(arr, l, i - 1, k);
+        }
+        if (i < k) {
+            quickSort(arr, i + 1, r, k);
+        }
+    }
+
+    private void swap(int[] arr, int x, int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
     }
 }
