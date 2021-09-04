4b9f5ad95121315e5cdd5e9df37b0b5f80837494
2293736867 <2293736867@qq.com>
2021-09-04 15:43:32 +0800

    LeetCode 剑指Offer10I

--- Solution/src/Main.java
+++ Solution/src/Main.java
@@ -13,10 +13,5 @@ public class Main {
     private final static Solution SOLUTION = new Solution();
 
     public static void main(String[] args) throws Exception {
-        int[] arr = Utils.randomArray(10_000);
-        int k = Math.min(100_00,arr.length);
-        Utils.print(arr);
-        Utils.print(k);
-//        Utils.print(SOLUTION.smallestK(arr,4));
     }
 }
