5f5918e8f1f4c069adf6baffa156cb76959f31f7
2293736867 <2293736867@qq.com>
2021-09-17 09:25:37 +0800

    LeetCode 36

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,31 +1,22 @@
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] row = new boolean[9][9];
-        boolean[][] col = new boolean[9][9];
-        boolean[][] diagonal = new boolean[9][9];
+        int[] row = new int[9];
+        int[] col = new int[9];
+        int[] diagonal = new int[9];
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] == '.') {
                     continue;
                 }
-                int index = board[i][j] - '1';
-                if (!row[i][index]) {
-                    row[i][index] = true;
-                } else {
-                    return false;
-                }
-                if (!col[j][index]) {
-                    col[j][index] = true;
-                } else {
-                    return false;
-                }
+                int index = 1 << board[i][j] - '1';
                 int k = ((i - i % 3) * 3 + j - j % 3) / 3;
-                if (!diagonal[k][index]) {
-                    diagonal[k][index] = true;
-                } else {
+                if ((row[i] & index) != 0 || (col[j] & index) != 0 || (diagonal[k] & index) != 0) {
                     return false;
                 }
+                row[i] |= index;
+                col[j] |= index;
+                diagonal[k] |= index;
             }
         }
         return true;
