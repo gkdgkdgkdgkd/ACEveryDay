6088b03261c79ecefea2395a7d04816e0e529283
2293736867 <2293736867@qq.com>
2021-10-06 09:51:58 +0800

    LeetCode 414

--- Solution/src/Main.java
+++ Solution/src/Main.java
@@ -9,9 +9,5 @@ public class Main {
     private static final Solution SOLUTION = new Solution();
 
     public static void main(String[] args) {
-//        Utils.print(SOLUTION.licenseKeyFormatting("--a-a-a-a--",2));
-//        Utils.print(SOLUTION.licenseKeyFormatting("r",1));
-//        Utils.print(SOLUTION.licenseKeyFormatting("aaaa",2));
-        Utils.print(SOLUTION.licenseKeyFormatting("a-a-a-a-",1));
     }
 }
