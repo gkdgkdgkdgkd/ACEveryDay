c83ce587a4ad3946929f3192815bb4a0e3c05301
2293736867 <2293736867@qq.com>
2021-10-24 22:12:02 +0800

    LeetCode 638

--- README.md
+++ README.md
@@ -197,3 +197,4 @@
 - [453](https://github.com/2293736867/ACEveryDay/tree/main/list/453)
 - [66](https://github.com/2293736867/ACEveryDay/tree/main/list/66)
 - [492](https://github.com/2293736867/ACEveryDay/tree/main/list/492)
+- [638](https://github.com/2293736867/ACEveryDay/tree/main/list/638)
--- Solution/src/Main.java
+++ Solution/src/Main.java
@@ -1,10 +1,30 @@
 import utils.Utils;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     private static final Solution SOLUTION = new Solution();
+
     public static void main(String[] args) {
-        Utils.print(SOLUTION.constructRectangle(12348));
+//        int n = 6;
+//        List<Integer> price = Arrays.stream(Utils.randomArray(n, 0, 10)).boxed().collect(Collectors.toList());
+//        int len = 100;
+//        List<List<Integer>> special = new ArrayList<>();
+//        for (int i = 0; i < len; i++) {
+//            List<Integer> list = Arrays.stream(Utils.randomArray(n, 0, 10)).boxed().collect(Collectors.toList());
+//            list.add(Utils.randomInt(0, 50));
+//            special.add(list);
+//        }
+//        List<Integer> needs = Arrays.stream(Utils.randomArray(n,0,10)).boxed().collect(Collectors.toList());
+//        Utils.print(price);
+//        Utils.print(special);
+//        Utils.print(needs);
+        Utils.print(SOLUTION.shoppingOffers(Utils.listOf(2, 3), Utils.listOf(
+                        Utils.listOf(1, 0, 1), Utils.listOf(0, 1, 2)
+                ),
+                Utils.listOf(1, 1)));
     }
 }
