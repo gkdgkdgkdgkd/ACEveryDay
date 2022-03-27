11a60e745bd68493588a62973ef337076bd11314
gkdgkdgkdgkd <4tixf3eb@anonaddy.me>
2022-03-28 00:43:45 +0800

    LeetCode 693

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,11 +1,5 @@
 public class Solution {
     public boolean hasAlternatingBits(int n) {
-        for (int next = n & 1; n > 0; next = n & 1) {
-            n >>= 1;
-            if (next == (n & 1)) {
-                return false;
-            }
-        }
-        return true;
+        return ((n ^ (n >> 1)) & ((n ^ (n >> 1)) + 1)) == 0;
     }
 }
