3d077c38fe8c609e1012ad3176327929600a5968
gkdgkdgkdgkd <2a3bd9e3-2b3c-40a3-9403-6b8c7210bf48@anonaddy.me>
2022-06-09 01:22:32 +0800

    clear

--- Solution/src/Main.java
+++ Solution/src/Main.java
@@ -3,7 +3,7 @@ import utils.Utils;
 import java.util.Scanner;
 
 public class Main {
-//    private static final Solution SOLUTION = new Solution();
+    private static final Solution SOLUTION = new Solution();
 
     public static void main(String[] args) {
     }
