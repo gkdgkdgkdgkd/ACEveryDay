3988042063cc1b20f239f91a9dd80515b9118dd2
gkdgkdgkdgkd <dbm28loo@anonaddy.me>
2022-07-02 10:41:17 +0800

    LeetCode 871

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,34 +1,30 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Solution {
-    private char[] cs;
-
-    public List<Integer> diffWaysToCompute(String s) {
-        cs = s.toCharArray();
-        return dfs(0, cs.length - 1);
-    }
-
-    private List<Integer> dfs(int l, int r) {
-        List<Integer> ans = new ArrayList<>();
-        for (int i = l; i <= r; i++) {
-            if (cs[i] >= '0' && cs[i] <= '9') continue;
-            List<Integer> l1 = dfs(l, i - 1), l2 = dfs(i + 1, r);
-            for (int a : l1) {
-                for (int b : l2) {
-                    int cur;
-                    if (cs[i] == '+') cur = a + b;
-                    else if (cs[i] == '-') cur = a - b;
-                    else cur = a * b;
-                    ans.add(cur);
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        LinkedList<int[]> queue = new LinkedList<>();
+        int start = 0;
+        int n = stations.length;
+        for (int i = 0; i < n && startFuel >= stations[i][0]; i++) {
+            start = i;
+            queue.add(new int[]{i, startFuel - stations[i][0]});
+        }
+        if (queue.isEmpty()) {
+            return -1;
+        }
+        ++start;
+        int res = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] front = queue.poll();
+                for (int j = start; j < n && front[1]+stations[front[0]][0] >= stations[j][0]; j++) {
+                    queue.add();
                 }
             }
         }
-        if (ans.isEmpty()) {
-            int cur = 0;
-            for (int i = l; i <= r; i++) cur = cur * 10 + (cs[i] - '0');
-            ans.add(cur);
-        }
-        return ans;
+        return res;
     }
 }
