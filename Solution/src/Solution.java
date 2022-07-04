7daa3dfe1de0f426a8badc210fa0db38fa1f8c60
gkdgkdgkdgkd <dbm28loo@anonaddy.me>
2022-07-04 11:55:46 +0800

    LeetCode 1200

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,42 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution {
-    public int nextGreaterElement(int nn) {
-        long n = nn;
-        int pre = (int) (n % 10);
-        n /= 10;
-        int[] count = new int[10];
-        ++count[pre];
-        while (n > 0) {
-            int next = (int) (n % 10);
-            n /= 10;
-            if (next < pre) {
-                ++count[next];
-                n *= 10;
-                for (int i = 1; i < 10; i++) {
-                    if (i > next && count[i] > 0) {
-                        --count[i];
-                        long sum = i;
-                        for (int j = 0; j < 10; j++) {
-                            while (count[j] > 0) {
-                                sum *= 10;
-                                sum += j;
-                                --count[j];
-                                n *= 10;
-                            }
-                        }
-                        long res = n + sum;
-                        if (res > Integer.MAX_VALUE) {
-                            return -1;
-                        } else {
-                            return (int) res;
-                        }
-                    }
-                }
-                return -1;
-            } else {
-                pre = next;
-                ++count[pre];
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(arr);
+        for (int i = 1, n = arr.length, min = Integer.MAX_VALUE; i < n; i++) {
+            if (arr[i] - arr[i - 1] < min) {
+                min = arr[i] - arr[i - 1];
+                res.clear();
+                res.add(List.of(arr[i - 1], arr[i]));
+            } else if (arr[i] - arr[i - 1] == min) {
+                res.add(List.of(arr[i - 1], arr[i]));
             }
         }
-        return -1;
+        return res;
     }
 }
