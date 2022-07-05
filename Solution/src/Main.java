913ead4f014feda40edc9f6ab315ee570b59a1eb
gkdgkdgkdgkd <dbm28loo@anonaddy.me>
2022-07-05 17:27:03 +0800

    LeetCode 729

--- Solution/src/Main.java
+++ Solution/src/Main.java
@@ -1,11 +1,12 @@
 import utils.Utils;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
 public class Main {
     private static final Solution SOLUTION = new Solution();
 
     public static void main(String[] args) {
+        MyCalendar calendar = new MyCalendar();
+        Utils.print(calendar.book(10, 20));
+        Utils.print(calendar.book(15, 25));
+        Utils.print(calendar.book(20, 30));
     }
 }
