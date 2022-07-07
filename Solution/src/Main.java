c19b25a2a915884f234cf1b356f839fab6f7bd6f
gkdgkdgkdgkd <dbm28loo@anonaddy.me>
2022-07-07 10:36:08 +0800

    LeetCode 648

--- Solution/src/Main.java
+++ Solution/src/Main.java
@@ -4,9 +4,5 @@ public class Main {
     private static final Solution SOLUTION = new Solution();
 
     public static void main(String[] args) {
-        MyCalendar calendar = new MyCalendar();
-        Utils.print(calendar.book(10, 20));
-        Utils.print(calendar.book(15, 25));
-        Utils.print(calendar.book(20, 30));
     }
 }
--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,85 +1,49 @@
-import java.util.*;
+import java.util.List;
 
 public class Solution {
-    private final Map<String, Deque<Integer>> scope = new HashMap<>();
-    private int start = 0;
+    private final Trie root = new Trie();
 
-    public int evaluate(String expression) {
-        return innerEvaluate(expression);
+    public String replaceWords(List<String> dictionary, String sentence) {
+        String[] words = sentence.split(" ");
+        for (String word : dictionary) {
+            insert(word);
+        }
+        StringBuilder builder = new StringBuilder();
+        for (String word : words) {
+            builder.append(find(word)).append(" ");
+        }
+        return builder.deleteCharAt(builder.length() - 1).toString();
     }
 
-    public int innerEvaluate(String expression) {
-        if (expression.charAt(start) != '(') { // 非表达式，可能为：整数或变量
-            if (Character.isLowerCase(expression.charAt(start))) {
-                String var = parseVar(expression); // 变量
-                return scope.get(var).peek();
-            } else { // 整数
-                return parseInt(expression);
-            }
-        }
-        int ret;
-        start++; // 移除左括号
-        if (expression.charAt(start) == 'l') { // "let" 表达式
-            start += 4; // 移除 "let "
-            List<String> vars = new ArrayList<>();
-            while (true) {
-                if (!Character.isLowerCase(expression.charAt(start))) {
-                    ret = innerEvaluate(expression); // let 表达式的最后一个 expr 表达式的值
-                    break;
-                }
-                String var = parseVar(expression);
-                if (expression.charAt(start) == ')') {
-                    ret = scope.get(var).peek(); // let 表达式的最后一个 expr 表达式的值
-                    break;
-                }
-                vars.add(var);
-                start++; // 移除空格
-                int e = innerEvaluate(expression);
-                scope.putIfAbsent(var, new ArrayDeque<>());
-                scope.get(var).push(e);
-                start++; // 移除空格
-            }
-            for (String var : vars) {
-                scope.get(var).pop(); // 清除当前作用域的变量
-            }
-        } else if (expression.charAt(start) == 'a') { // "add" 表达式
-            start += 4; // 移除 "add "
-            int e1 = innerEvaluate(expression);
-            start++; // 移除空格
-            int e2 = innerEvaluate(expression);
-            ret = e1 + e2;
-        } else { // "mult" 表达式
-            start += 5; // 移除 "mult "
-            int e1 = innerEvaluate(expression);
-            start++; // 移除空格
-            int e2 = innerEvaluate(expression);
-            ret = e1 * e2;
-        }
-        start++; // 移除右括号
-        return ret;
+    private static class Trie {
+        boolean end = false;
+        Trie[] children = new Trie[26];
     }
 
-    public int parseInt(String expression) {
-        int n = expression.length();
-        int ret = 0, sign = 1;
-        if (expression.charAt(start) == '-') {
-            sign = -1;
-            start++;
+    private void insert(String str) {
+        Trie trie = root;
+        for (char c : str.toCharArray()) {
+            if (trie.children[c - 'a'] == null) {
+                trie.children[c - 'a'] = new Trie();
+            }
+            trie = trie.children[c - 'a'];
         }
-        while (start < n && Character.isDigit(expression.charAt(start))) {
-            ret = ret * 10 + (expression.charAt(start) - '0');
-            start++;
-        }
-        return sign * ret;
+        trie.end = true;
     }
 
-    public String parseVar(String expression) {
-        int n = expression.length();
-        StringBuilder ret = new StringBuilder();
-        while (start < n && expression.charAt(start) != ' ' && expression.charAt(start) != ')') {
-            ret.append(expression.charAt(start));
-            start++;
+    private String find(String str) {
+        Trie trie = root;
+        StringBuilder builder = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            if (trie.end) {
+                return builder.toString();
+            }
+            if (trie.children[c - 'a'] == null) {
+                return str;
+            }
+            builder.append(c);
+            trie = trie.children[c - 'a'];
         }
-        return ret.toString();
+        return str;
     }
 }
