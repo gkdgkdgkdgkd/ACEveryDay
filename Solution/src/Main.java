62257cf9152b41ca08233193c239d228c1d1949f
gkdgkdgkdgkd <dbm28loo@anonaddy.me>
2022-07-15 13:33:15 +0800

    LeetCode 558

--- Solution/src/Main.java
+++ Solution/src/Main.java
@@ -4,7 +4,5 @@ public class Main {
     private static final Solution SOLUTION = new Solution();
 
     public static void main(String[] args) {
-        WordFilter filter = new WordFilter(Utils.array("apple"));
-        Utils.print(filter.f("b","e"));
     }
 }
